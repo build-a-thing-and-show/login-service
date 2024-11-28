3b57a5ef1e77251614fbb5f73e771aab191750bf
Reza <rzgholizadeh@gmail.com>
2024-11-28 14:48:44 -0800

    Rewrite the test

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,21 +4,26 @@ import app from '../src/index';
 
 jest.mock('../src/index', () => ({
   default: jest.fn().mockReturnValue({
-    get: jest.fn<Express['get']>(),
+    get: jest.fn(),
     listen: jest.fn(),
   }),
 }));
 
 describe('GET /', () => {
   it('should return "This Application is under construction. Please be patient."', async () => {
-    const mockRequestHandler = jest.fn();
+    const mockResponse: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+
     (app.get as jest.Mock).mockImplementation((path: string, handler: RequestHandler) => {
-      if (path === '/hello') {
+      if (path === '/') {
         handler({ query: {} } as Request, mockResponse as Response, jest.fn() as NextFunction);
       }
     });
+
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('should return "This Application is under construction. Please be patient.');
+    expect(response.text).toBe('This Application is under construction. Please be patient.');
   });
 });
