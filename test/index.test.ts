9d92679065884b5c1ac733ec30107dde3ae80bdd
Reza <rzgholizadeh@gmail.com>
2024-11-28 14:42:15 -0800

    Update library

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express-serve-static-core';
 import app from '../src/index';
 
 jest.mock('../src/index', () => ({
@@ -13,8 +13,8 @@ describe('GET /', () => {
   it('should return "This Application is under construction. Please be patient."', async () => {
     const mockRequestHandler = jest.fn();
     (app.get as jest.Mock).mockImplementation((path: string, handler: RequestHandler) => {
-      if (path === '/') {
-        handler({ query: {} } as Request, { send: jest.fn() } as Response, jest.fn() as NextFunction);
+      if (path === '/hello') {
+        handler({ query: {} } as Request, mockResponse as Response, jest.fn() as NextFunction);
       }
     });
     const response = await request(app).get('/');
