e509c1f51b6772a809cd352452a98874ea3679ad
Reza <rzgholizadeh@gmail.com>
2024-11-28 14:34:25 -0800

    Use correct types

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,19 @@
 import request from 'supertest';
+import express, { Express, RequestHandler } from 'express';
 import app from '../src/index';
 
 jest.mock('../src/index', () => ({
   default: jest.fn().mockReturnValue({
-    get: jest.fn(),
+    get: jest.fn<Express['get']>(),
     listen: jest.fn(),
   }),
 }));
 
 describe('GET /', () => {
   it('should return "This Application is under construction. Please be patient."', async () => {
-    app.get.mockImplementation((path, handler) => {
-      if (path === '/') {
+    const mockRequestHandler = jest.fn();
+    (app.get as jest.Mock).mockImplementation((path: string, handler: RequestHandler) => {
+      if (path === '/hello') {
         handler({ query: {} }, { send: jest.fn() });
       }
     });
