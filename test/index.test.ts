9961064855d1847f7ed005c31de545e62a7e6359
Reza <rzgholizadeh@gmail.com>
2024-11-28 15:45:50 -0800

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,23 +2,17 @@ import request from 'supertest';
 import express, { Express, Request, Response } from 'express';
 import app from '../src/index';
 
-jest.mock('express', () => ({
-  Router: jest.fn().mockReturnThis(),
-  Request: jest.fn(),
-  Response: jest.fn(),
-}));
-
-jest.mock('../src/index', () => ({
-  default: {
-    get: jest.fn(),
-  },
-}));
-
 describe('GET /', () => {
-  it('Should return the correct message for the GET method', async () => {
+  it('should respond with "Hello, World!"', async () => {
+    const mockResponse = {
+      send: jest.fn(),
+    };
+
     app.get.mockImplementation((_, res) => {
-      res.send('Incorrect value');
+      res = mockResponse; // Assign the mock response object to the res parameter
+      res.send('Hello, World!');
     });
+
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
