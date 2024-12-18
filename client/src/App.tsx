74f872d48ce2b46603842bb4bb9e9895c4c34bee
Amin <aminghz@gmail.com>
2024-12-17 19:10:46 -0800

    wip. first simple landing page version.

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Welcome to My Landing Page</h1>
+      <p>This is a minimal Express + React setup! 🚀</p>
+    </div>
+  );
+};
+
+export default App;
