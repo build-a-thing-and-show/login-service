517bd3ac7eaaf3ed107f75d63f05284429ae33ef
Reza <rzgholizadeh@gmail.com>
2025-01-21 21:25:54 -0800

    remove old implementation

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import request from 'supertest';
-import app from '../src/index';
-
-describe('GET /', () => {
-  it('should respond with "Hello, World!"', () => {
-    expect('test1').toBe('test1');
-  })
-});
